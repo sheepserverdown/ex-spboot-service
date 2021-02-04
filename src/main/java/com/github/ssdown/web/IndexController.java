950e5874059608cba91a630fcf70fc698f77b737
sheepserverdown <sheepserverdown@github.com>
2021-02-04 13:53:56 +0900

    test 모듈에 Spring Security 적용 / 통과하게 설정 변경 부가적으로 EnableJpaAuditing 을 Application 전역 적용이 아닌 별도의 Config로 분리함

--- src/main/java/com/github/ssdown/web/IndexController.java
+++ src/main/java/com/github/ssdown/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.github.ssdown.web;
 
+import com.github.ssdown.web.config.auth.LoginUser;
 import com.github.ssdown.web.config.auth.dto.SessionUser;
 import com.github.ssdown.web.dto.PostsResponseDto;
 import com.github.ssdown.web.service.posts.PostsService;
@@ -18,10 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null) {
             model.addAttribute("userName", user.getName());
         }
