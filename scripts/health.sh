b2cce91090973bb0e763c60cf28e79022fa0677e
sheepserverdown <sheepserverdown@gmail.com>
2021-02-18 20:49:19 +0900

    deploy test

--- scripts/health.sh
+++ scripts/health.sh
@@ -21,7 +21,7 @@ do
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 존재하는 경우)
     echo "> Health check 성공"
-    switch_proxy  # 잘 떴는지 확인해야 Nginx 프록시 설정을 변경(switch_proxy) 한다.
+    switch_proxy  # 잘 떴는지 확인해야 Nginx 프록시 설정을 변경(switch_proxy) 한다
     break
   else
     echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
